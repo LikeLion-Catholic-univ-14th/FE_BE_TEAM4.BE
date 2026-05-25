bb24debb4892dc5e164233a7383bf63f2c670dfc
githeobu <heominji1012@gmail.com>
2026-05-25 22:41:01 +0900

    초기 세팅

---
+++ src/main/java/com/example/bf_be/BfBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.bf_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BfBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bf_be/BfBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bf_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BfBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
